9a3be11efe810ea5c7f26dfd2accd03df71e95a2
barrfalk <bfalk@salussystems.com>
2022-12-12 14:10:19 -0800

    Update main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,6 +5,7 @@ import { join } from 'path';
 
 async function bootstrap () {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors();
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setViewEngine('html');
 
